4a4bb99ad4a80ffaecc884bac709f00e90e16d7c
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2026-05-26 19:46:58 +0300

    2

--- js/slick.js
+++ js/slick.js
@@ -1,22 +1,5 @@
 $(function () {
-  $(".class-gallery-list").slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: "linear",
-    prevArrow: ".class-gallery-list-left",
-    nextArrow: ".class-gallery-list-right",
-  });
-});
-
-$(function () {
-  $(".class-reviews-list").slick({
+  $(".p-o-why-list").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -27,7 +10,56 @@ $(function () {
     dots: true,
     centerMode: true,
     cssEase: "linear",
-    prevArrow: ".class-reviews-list-left",
-    nextArrow: ".class-reviews-list-right",
+    prevArrow: ".p-o-why-list-left",
+    nextArrow: ".p-o-why-list-right",
+  });
+});
+
+$(function () {
+  $(".p-o-uniqueness-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".p-o-uniqueness-list-left",
+    nextArrow: ".p-o-uniqueness-list-right",
+  });
+});
+$(function () {
+  $(".p-o-how-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".p-o-how-list-left",
+    nextArrow: ".p-o-how-list-right",
+  });
+});
+$(function () {
+  $(".p-o-reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".p-o-reviews-list-left",
+    nextArrow: ".p-o-reviews-list-right",
   });
 });
